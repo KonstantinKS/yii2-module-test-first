72f5bd8ca5e640f474f6266589c7ca3fb5253939
Konstantin Karpov <k-karpov@inbox.ru>
2019-09-12 09:26:54 +0300

    Добавлен контролер RSL документа

---
+++ src/views/rsl-document/render-static.php
@@ -0,0 +1,38 @@
+<?php
+    var_dump(Yii::getAlias('@KonstantinKS/test1'));
+?>
+
+<?php
+    echo '<pre>';
+    print_r($identifier);
+    echo '</pre>';
+?>
+
+<?php
+    echo '<pre>';
+    print_r($render);
+    echo '</pre>';
+?>
+
+<?php
+    echo '<pre>';
+    print_r($pagesCount);
+    echo '</pre>';
+?>
+
+<?php
+    echo '<pre>';
+    var_dump($resources);
+    echo '</pre>';
+?>
+
+<?php
+    echo '<pre>';
+    var_dump($documentCard);
+    echo '</pre>';
+?>
+
+
+
+
+
